13b3bece5621397fe6074cd3bf0f24b69b1ee3a8
Natalie Baker <technology@notvery.moe>
2023-12-24 13:55:13 +0800

    Add angle test

--- crates/level/src/map/angle.rs
+++ crates/level/src/map/angle.rs
@@ -1,9 +1,31 @@
 // Copyright 2023 Natalie Baker // AGPLv3 //
 
-pub fn to_world_angle(v: u16) -> f32 {
+pub fn to_world_angle(v: i16) -> f32 {
     std::f32::consts::TAU/(v as f32)
 }
 
-pub fn from_world_angle(v: f32) -> u16 {
-    (std::f32::consts::TAU/v).rem_euclid(u16::MAX as f32) as u16
+pub fn from_world_angle(v: f32) -> i16 {
+    ((std::f32::consts::TAU/v).rem_euclid(i16::MAX as f32)) as i16
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{from_world_angle, to_world_angle};
+
+    #[test]
+    fn round_trip() {
+        let do_test = |v: i16| assert_eq!(v.rem_euclid(i16::MAX), from_world_angle(to_world_angle(v)));
+
+        do_test(0);
+
+        do_test(-1);
+        do_test( 1);
+
+        do_test(i16::MAX);
+        do_test(i16::MIN);
+
+        do_test(i16::MAX-1);
+        do_test(i16::MIN+1);
+    }
+
 }
