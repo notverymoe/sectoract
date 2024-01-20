6760157ce01207c124d54bb939d64d6d8e81d7ea
Natalie Baker <technology@notvery.moe>
2024-01-20 11:14:43 +0800

    Remove old code

--- crates/level2/src/map/sector/nested.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 Natalie Baker // AGPLv3 //
-
-use crate::map::{Transform, IdentifierSector};
-
-#[derive(Debug, Clone, Copy)]
-pub struct SectorNested {
-    pub sector:    IdentifierSector,
-    pub transform: Transform,
-}
--- crates/level3/src/util/mod.rs
+++
@@ -1,5 +0,0 @@
-// Copyright 2023 Natalie Baker // AGPLv3 //
-
-mod section_combine;
-pub use section_combine::*;
-
